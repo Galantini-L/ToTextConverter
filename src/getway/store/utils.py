5bda5d2163105a67c3e5fd8acef4f829b90727e3
Galantini-L <galantini99@gmail.com>
2023-08-01 10:53:50 -0300

    First commit. Aws strucure

--- src/getway/store/utils.py
+++ src/getway/store/utils.py
@@ -7,8 +7,27 @@ def upload(file, fs, sqsChannel, access):
         file {_type_} -- file to be updated
         fs {_type_} -- gridFS Instance
         sqsChannel {_type_} -- AWS SQS channel
-        access {_type_} -- user access information
+        access {_type_} -- user access information    
+    
+    {
+        "Version": "2012-10-17",
+        "Id": "__default_policy_ID",
+        "Statement": [
+            {
+            "Sid": "__owner_statement",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "917414156098"
+            },
+            "Action": [
+                "SQS:*"
+            ],
+            "Resource": "arn:aws:sqs:us-east-2:917414156098:image-to-convert-queue.fifo"
+            }
+        ]
+    }
     """
 
+    
 
-print(upload.__doc__)
+    
